d81b05bbbfc357c984f789041c41adff93e41cb2
GHW666666 <3121906711@qq.com>
2024-11-05 22:17:00 +0800

    es6 and python

---
+++ js/flower/es6/let-const/js1.js
@@ -0,0 +1,21 @@
+function sayHello(){
+let name = "g";
+for(let i = 0;i < 2;i++){
+    console.log("Hello " + name + ".");
+}
+if(i==1){confirm("wokao");
+
+}
+}
+//es5 var全局变量声明
+var age = 25;
+if(age > 5){
+    //es6新增的let 变量声明 2015年
+    var name = "Y";
+    let dogyears = age * 7;
+    console.log("Y.");
+}
+//代码块，块级作用域
+sayHello();
+console.log(name,"name");
+
---
+++ js/flower/es6/let-const/js2.js
@@ -0,0 +1,10 @@
+// 常量大写
+{
+    const PI=3.1415926;
+}PI=3.14;
+const person={
+    name:'zhangsan',
+    age:18
+}
+person.name='lisi';
+console.log(person.name);
