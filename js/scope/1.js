cb5c88ee8f3dd536fda796c1b7a3965af36e182b
GHW666666 <3121906711@qq.com>
2024-11-12 22:02:03 +0800

    LESSON_HM

---
+++ js/scope/1.js
@@ -0,0 +1,13 @@
+var a=1;
+var hash=new Map();
+args=[[1,2],[2,5],[3,4]];
+var item=args.join(",");
+hash.set(item,1);
+console.log(hash);
+console.log(hash.size);
+
+args2=[[1,2],[3,4]];
+var item2=args.join(",");
+hash.set(item2,2);
+console.log(hash);
+console.log(hash.size);
