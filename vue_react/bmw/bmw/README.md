a272f5faf9777543cf99922b1c6e88cd737b7d3a
ghw666666 <3121906711@qq.com>
2025-01-02 21:34:52 +0800

    lifecycle

---
+++ vue_react/bmw/bmw/README.md
@@ -0,0 +1,8 @@
+# v-if/v-show
+- v-if 条件渲染 
+只有为真才会渲染在dom树和挂载在组件树
+- v-if true=> false销毁
+- v-if false=> true 
+- v-show 一定渲染，但条件为假时把dom的display设置成none
+
+- 组件是一个类
---
+++ vue_react/bmw/bmw/src/components/LifecycleComponent.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+      <div>
+        <p id="ghw">LifecycleComponent-Count:{{count}}</p>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  import { defineProps,
+    onBeforeMount,   
+    onMounted,
+    onBeforeUnmount,
+    onUnmounted,
+    onBeforeUpdate,
+    onUpdated } from 'vue';
+  defineProps({
+    count: {
+      type: Number,
+      required: true
+    }
+  })
+  onBeforeMount(() => {
+        console.log('beforeMounted');
+        console.log(document.getElementById('ghw'));
+    })
+  console.log('hhh');
+    onMounted(() => {
+        console.log('mounted');
+    })
+  onBeforeMount(() => {
+        console.log('beforeUnmounted');
+    })
+  onMounted(() => {
+        console.log('unmounted');
+    })
+onBeforeUpdate(() => {
+        console.log('beforeUpdate');
+    })
+  onUpdated(() => {
+        console.log('updated');
+    })
+  </script>
+  
+  <style scoped>
+  
+  </style>
